adf4f1a882475bc9ef5b8f8d04d5eae100dd66bc
dudeperf3ct <dudeperf3ct@users.noreply.github.com>
2023-03-21 15:44:34 +0530

    add test for run and provision command

---
+++ tests/test_cli/test_provision.py
@@ -0,0 +1,15 @@
+"""Test suite to test the provision command and all it's subcommands."""
+from matcha_ml import __version__
+from matcha_ml.cli.cli import app
+
+
+def test_cli_provision_command(runner):
+    """Test cli for provision command."""
+    # Invoke provision command
+    result = runner.invoke(app, ["provision", "--help"])
+
+    # Exit code 0 means there was no error
+    assert result.exit_code == 0
+
+    # Assert string is present in cli output
+    assert "Provision cloud resources." in result.stdout
--- tests/test_cli/test_run.py
+++ tests/test_cli/test_run.py
@@ -1,9 +1,27 @@
-# """Test suite to test the CLI."""
-# from matcha_ml.cli.cli import app
-# from matcha_ml import __version__
+"""Test suite to test the run command and all it's subcommands."""
+from matcha_ml import __version__
+from matcha_ml.cli.cli import app
 
 
-# def test_cli_no_args(runner):
-#     result = runner.invoke(app, ["run", "--help"])
-#     assert result.exit_code == 0
-#     assert "CLI base command for matcha" in result.stdout
+def test_cli_run_command(runner):
+    """Test cli for run command."""
+    # Invoke run command
+    result = runner.invoke(app, ["run", "--help"])
+
+    # Exit code 0 means there was no error
+    assert result.exit_code == 0
+
+    # Assert string is present in cli output
+    assert "Run command." in result.stdout
+
+
+def test_cli_train_command(runner):
+    """Test cli for run command."""
+    # Invoke run command
+    result = runner.invoke(app, ["run", "train", "--help"])
+
+    # Exit code 0 means there was no error
+    assert result.exit_code == 0
+
+    # Assert string is present in cli output
+    assert "Run train subcommand." in result.stdout
