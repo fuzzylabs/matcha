7add02e98fc834b81f8e53b4556be6a364278e3a
dudeperf3ct <dudeperf3ct@users.noreply.github.com>
2023-03-22 13:31:41 +0530

    remove defaults

--- src/infrastructure/variables.tf
+++ src/infrastructure/variables.tf
@@ -5,29 +5,24 @@ variable "prefix" {
 
 variable "location" {
   description = "The Azure Region in which all resources should be provisioned"
-  default     = "uksouth"
 }
 
 variable "mysql_username" {
   description = "Username to connect to Azure MySQL server"
-  default     = "usermatcha"
 }
 
 variable "mysql_password" {
   description = "Password to connect to Azure MySQL server"
   sensitive   = true
-  default     = "mysqladminun"
 }
 
 variable "mlflow_username" {
   type        = string
   description = "Usernmame to set to access mlflow server"
-  default     = "usermatcha"
 }
 
 variable "mlflow_password" {
   type        = string
   description = "Password to set to access mlflow server"
   sensitive   = true
-  default     = "mlflowadmin"
 }
