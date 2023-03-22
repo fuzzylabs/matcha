a3aef7634f21d5f928a807d8e236d5dffd75ff45
D. Lowl <d.lowl@d-lowl.space>
2023-03-22 13:55:24 +0400

    Copy the configuration files to the project directory

--- src/matcha_ml/cli/cli.py
+++ src/matcha_ml/cli/cli.py
@@ -4,12 +4,26 @@ from typing import Optional
 import typer
 
 from matcha_ml import __version__
-from matcha_ml.cli import provision, run
+from matcha_ml.cli import run
+from matcha_ml.cli.provision import provision_resources
 
 app = typer.Typer(no_args_is_help=True)
 
-# Create a group for all subcommands
-app.add_typer(provision.app, name="provision", help="Provision cloud resources.")
+
+@app.command()
+def provision(
+    template: Optional[str] = typer.Option(
+        None, help="Template to use for provisioning."
+    )
+) -> None:
+    """Provision cloud resources with a template.
+
+    Args:
+        template (str, optional): Path to template.
+                                Defaults to typer.Option(..., help="Template to use for provisioning.").
+    """
+    provision_resources(template)
+
 
 # Create a group for all subcommands
 app.add_typer(run.app, name="run", help="Run command.")
@@ -40,6 +54,9 @@ def cli(
     Run 'matcha <command> --help' for more information on a specific command.
 
     For more help on how to use matcha, head to https://docs.mlops.wtf
+
+    Args:
+        version (bool, optional): matcha version flag
     """
     pass
 
--- src/matcha_ml/cli/provision.py
+++ src/matcha_ml/cli/provision.py
@@ -1,6 +1,7 @@
 """Provision CLI."""
 import dataclasses
 import os
+from shutil import copy
 from typing import Optional
 
 import typer
@@ -50,23 +51,46 @@ def build_template(
     print(f"[green]Ensure template destination directory: {destination}[/green]")
     # TODO test how error for directory cannot be created is handled?
 
+    submodule_filenames = ["main.tf", "variables.tf", "output.tf"]
+
+    main_module_filenames = [
+        ".gitignore",
+        ".terraform.lock.hcl",
+    ] + submodule_filenames
+
+    submodule_names = ["aks", "resource_group"]
     # TODO copy template to destination
+    print("Copying root module configuration...")
+    for filename in main_module_filenames:
+        source_path = os.path.join(template_src, filename)
+        destination_path = os.path.join(destination, filename)
+        copy(source_path, destination_path)
+
+    for submodule_name in submodule_names:
+        print(f"Copying {submodule_name} module configuration...")
+        os.makedirs(os.path.join(destination, submodule_name), exist_ok=True)
+        for filename in submodule_filenames:
+            source_path = os.path.join(template_src, submodule_name, filename)
+            destination_path = os.path.join(destination, submodule_name, filename)
+            copy(source_path, destination_path)
+    print("[green]Configuration was copied[/green]")
+
     # TODO write template variables to destination
 
 
-@app.command()
-def provision(
-    template: Optional[str] = typer.Option(
-        None, help="Template to use for provisioning."
-    )
-) -> None:
+def provision_resources(template: Optional[str] = None) -> None:
     """Provision cloud resources with a template.
 
     Args:
         template (str, optional): Path to template.
-                                Defaults to typer.Option(..., help="Template to use for provisioning.").
     """
     project_directory = os.getcwd()
     destination = os.path.join(project_directory, ".matcha", "infrastructure")
+
+    if template is None:
+        template = os.path.join(
+            os.path.dirname(__file__), os.pardir, os.pardir, "infrastructure"
+        )
+
     config = build_template_configuration()
-    build_template(config, "", destination)
+    build_template(config, template, destination)
