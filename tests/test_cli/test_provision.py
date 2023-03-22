b44de29fb3f3d2226e6d1399964d7266162634be
D. Lowl <d.lowl@d-lowl.space>
2023-03-22 17:27:35 +0400

    Add test cases for running the provision command

--- tests/test_cli/test_provision.py
+++ tests/test_cli/test_provision.py
@@ -1,8 +1,8 @@
 """Test suite to test the provision command and all its subcommands."""
 import json
 import os
-import shutil
 import tempfile
+from typing import Dict, Generator
 
 import pytest
 
@@ -12,61 +12,18 @@ from src.matcha_ml.cli.provision import TemplateVariables, build_template
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-@pytest.fixture
-def matcha_testing_folder():
-    """A fixture for creating and removing the .matcha folder for storing and moving files.
-
-    Yields:
-        dir: a folder for temporarily storing and moving files from tests.
-    """
-    temp_dir = tempfile.TemporaryDirectory()
-
-    # tests are executed at this point
-    yield temp_dir.name
-
-    # delete temp folder
-    temp_dir.cleanup()
-
-
-@pytest.fixture
-def template_src_path():
-    """_summary_"""
-    template_dir = os.path.join(BASE_DIR, os.pardir, os.pardir, "src", "infrastructure")
-
-    return template_dir
-
-
-def test_cli_provision_command_help(runner):
-    """Test cli for provision command help."""
-    # Invoke provision command
-    result = runner.invoke(app, ["provision", "--help"])
-
-    # Exit code 0 means there was no error
-    assert result.exit_code == 0
-
-    # Assert string is present in cli output
-    assert "Provision cloud resources with a template." in result.stdout
-
-
-def test_build_template(matcha_testing_folder, template_src_path):
-    """_summary_
+def assert_infrastructure(destination_path: str, expected_tf_vars: Dict[str, str]):
+    """Assert if the infrastructure configuration is valid
 
     Args:
-        matcha_testing_folder (str): Temporary .matcha directory path
-        template_src_path (str): Existing template directory path
+        destination_path (str): infrastructure config destination path
+        expected_tf_vars (Dict[str, str]): expected Terraform variables
     """
-    config = TemplateVariables("uksouth")
-
-    destination_path = os.path.join(matcha_testing_folder, "infrastructure")
-
-    build_template(config, template_src_path, destination_path)
-
     module_file_names = ["main.tf", "variables.tf", "output.tf"]
 
     module_names = ["aks", "resource_group"]
 
     # Test that destination path is a directory
-    print(destination_path)
     assert os.path.exists(destination_path)
 
     for module_file_name in module_file_names:
@@ -83,9 +40,117 @@ def test_build_template(matcha_testing_folder, template_src_path):
     variables_file_path = os.path.join(destination_path, "terraform.tfvars.json")
     assert os.path.exists(variables_file_path)
 
-    expected_tf_vars = {"location": "uksouth", "prefix": "matcha"}
-
     with open(variables_file_path, "r") as f:
         tf_vars = json.load(f)
 
     assert tf_vars == expected_tf_vars
+
+
+@pytest.fixture
+def matcha_testing_directory() -> Generator[str]:
+    """A fixture for creating and removing temporary test directory for storing and moving files.
+
+    Yields:
+        str: a path to temporary directory for storing and moving files from tests.
+    """
+    temp_dir = tempfile.TemporaryDirectory()
+
+    # tests are executed at this point
+    yield temp_dir.name
+
+    # delete temp folder
+    temp_dir.cleanup()
+
+
+@pytest.fixture
+def template_src_path() -> str:
+    """Fixture for the test infrastructure template path.
+
+    Returns:
+        str: template path
+    """
+    template_dir = os.path.join(BASE_DIR, os.pardir, os.pardir, "src", "infrastructure")
+
+    return template_dir
+
+
+def test_cli_provision_command_help(runner):
+    """Test cli for provision command help.
+
+    Args:
+        runner (CliRunner): typer CLI runner
+    """
+    # Invoke provision command
+    result = runner.invoke(app, ["provision", "--help"])
+
+    # Exit code 0 means there was no error
+    assert result.exit_code == 0
+
+    # Assert string is present in cli output
+    assert "Provision cloud resources with a template." in result.stdout
+
+
+def test_cli_provision_command(runner, matcha_testing_directory):
+    """Test provision command to copy the infrastructure template.
+
+    Args:
+        runner (CliRunner): typer CLI runner
+        matcha_testing_directory (str): temporary working directory
+    """
+    os.chdir(matcha_testing_directory)
+
+    # Invoke provision command
+    result = runner.invoke(app, ["provision"], input="\nuksouth\n")
+
+    # Exit code 0 means there was no error
+    assert result.exit_code == 0
+
+    destination_path = os.path.join(
+        matcha_testing_directory, ".matcha", "infrastructure"
+    )
+
+    expected_tf_vars = {"location": "uksouth", "prefix": "matcha"}
+
+    assert_infrastructure(destination_path, expected_tf_vars)
+
+
+def test_cli_provision_command_with_prefix(runner, matcha_testing_directory):
+    """Test provision command to copy the infrastructure template.
+
+    Args:
+        runner (CliRunner): typer CLI runner
+        matcha_testing_directory (str): temporary working directory
+    """
+    os.chdir(matcha_testing_directory)
+
+    # Invoke provision command
+    result = runner.invoke(app, ["provision"], input="coffee\nukwest\n")
+
+    # Exit code 0 means there was no error
+    assert result.exit_code == 0
+
+    destination_path = os.path.join(
+        matcha_testing_directory, ".matcha", "infrastructure"
+    )
+
+    expected_tf_vars = {"location": "ukwest", "prefix": "coffee"}
+
+    assert_infrastructure(destination_path, expected_tf_vars)
+
+
+def test_build_template(matcha_testing_directory, template_src_path):
+    """Test that the template is built and copied to correct locations.
+
+    Args:
+        matcha_testing_directory (str): Temporary .matcha directory path
+        template_src_path (str): Existing template directory path
+    """
+    config = TemplateVariables("uksouth")
+
+    destination_path = os.path.join(matcha_testing_directory, "infrastructure")
+
+    build_template(config, template_src_path, destination_path)
+
+    expected_tf_vars = {"location": "uksouth", "prefix": "matcha"}
+
+    assert_infrastructure(destination_path, expected_tf_vars)
