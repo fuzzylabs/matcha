4b3cf0d196d3c72b56c468544da3481cf547b38b
dudeperf3ct <dudeperf3ct@users.noreply.github.com>
2023-03-24 13:16:06 +0530

    fix test for real

--- tests/test_cli/test_provision.py
+++ tests/test_cli/test_provision.py
@@ -90,8 +90,8 @@ def test_cli_provision_command(runner, matcha_testing_directory):
     # Invoke provision command
     result = runner.invoke(app, ["provision"], input="\nuksouth\nno\n")
 
-    # Exit code 0 means there was no error
-    assert result.exit_code == 0
+    # Exit code 1 exists because we add a prompt "no" to provision resources
+    assert result.exit_code == 1
 
     destination_path = os.path.join(
         matcha_testing_directory, ".matcha", "infrastructure"
@@ -116,8 +116,8 @@ def test_cli_provision_command_with_args(runner, matcha_testing_directory):
         app, ["provision", "--location", "uksouth", "--prefix", "matcha"], input="no\n"
     )
 
-    # Exit code 0 means there was no error
-    assert result.exit_code == 0
+    # Exit code 1 exists because we add a prompt "no" to provision resources
+    assert result.exit_code == 1
 
     destination_path = os.path.join(
         matcha_testing_directory, ".matcha", "infrastructure"
@@ -140,8 +140,8 @@ def test_cli_provision_command_with_prefix(runner, matcha_testing_directory):
     # Invoke provision command
     result = runner.invoke(app, ["provision"], input="coffee\nukwest\nno\n")
 
-    # Exit code 0 means there was no error
-    assert result.exit_code == 0
+    # Exit code 1 exists because we add a prompt "no" to provision resources
+    assert result.exit_code == 1
 
     destination_path = os.path.join(
         matcha_testing_directory, ".matcha", "infrastructure"
