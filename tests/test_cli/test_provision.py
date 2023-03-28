b49f03eb44d1b8562009df3678c8acea015aa944
dudeperf3ct <dudeperf3ct@users.noreply.github.com>
2023-03-28 19:14:29 +0530

    additional test

--- tests/test_cli/test_provision.py
+++ tests/test_cli/test_provision.py
@@ -136,6 +136,30 @@ def test_cli_provision_command_with_prefix(runner, matcha_testing_directory):
     assert_infrastructure(destination_path, expected_tf_vars)
 
 
+def test_cli_provision_command_with_default_prefix(runner, matcha_testing_directory):
+    """Test provision command to copy the infrastructure template with no prefix.
+
+    Args:
+        runner (CliRunner): typer CLI runner
+        matcha_testing_directory (str): temporary working directory
+    """
+    os.chdir(matcha_testing_directory)
+
+    # Invoke provision command
+    result = runner.invoke(app, ["provision"], input="ukwest\n\nno\n")
+
+    # Exit code 0 means there was no error
+    assert result.exit_code == 0
+
+    destination_path = os.path.join(
+        matcha_testing_directory, ".matcha", "infrastructure"
+    )
+
+    expected_tf_vars = {"location": "ukwest", "prefix": "matcha"}
+
+    assert_infrastructure(destination_path, expected_tf_vars)
+
+
 def test_cli_provision_command_with_verbose_arg(runner, matcha_testing_directory):
     """Test that the verbose argument works and provision shows more output.
 
