03e3a9bab40f3837023df7d4d7b7e97d41d2024c
D. Lowl <d.lowl@d-lowl.space>
2023-03-28 11:43:57 +0400

    Factor reuse function out

--- src/matcha_ml/cli/provision.py
+++ src/matcha_ml/cli/provision.py
@@ -8,35 +8,12 @@ from rich import print
 from matcha_ml.templates.build_templates.azure_template import (
     build_template,
     build_template_configuration,
+    reuse_configuration,
 )
 from matcha_ml.templates.run_template import TerraformService
 
 # create a typer app to group all provision subcommands
 app = typer.Typer()
-
-
-def reuse_configuration(path: str) -> bool:
-    """Check if a configuration already a use, and prompt user to override or reuse it.
-
-    Args:
-        path (str): path to the infrastructure configuration
-
-    Returns:
-        bool: decision to reuse the existing configuration
-    """
-    if os.path.exists(path):
-        summary_message = """The following resources are already configured for provisioning:
-1. [yellow] Resource group [/yellow]: A resource group
-2. [yellow] Azure Kubernetes Service (AKS) [/yellow]: A kubernetes cluster
-3. [yellow] Azure Storage Container [/yellow]: A storage container
-"""
-        print(summary_message)
-
-        return not typer.confirm(
-            "Do you want to override the configuration? Otherwise, the existing configuration will be reused"
-        )
-    else:
-        return False
 
 
 def provision_resources(
