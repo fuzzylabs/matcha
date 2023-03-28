b33bad3b33fd197df4461b2bde47dc4359f216b2
Jonathan Carlton <jonathan.carlton@hotmail.com>
2023-03-28 09:44:28 +0100

    enhancement suggested in PR comment

--- src/matcha_ml/templates/run_template.py
+++ src/matcha_ml/templates/run_template.py
@@ -149,7 +149,8 @@ class TerraformService:
             f"Are you happy for these resources to be {verb} (y/N; yes/No)?",
             type=str,
         ).lower()
-        return True if prompt == "yes" or prompt == "y" else False
+        # return True if prompt == "yes" or prompt == "y" else False
+        return bool(prompt == 'yes' or prompt == 'y')
 
     def _init_and_apply(self) -> None:
         """Run terraform init and apply to create resources on cloud.
