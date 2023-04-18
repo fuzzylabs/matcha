f0b8fd5701c50dc394c04e22cde0511c0372fca5
Oscar <osw282@gmail.com>
2023-04-18 16:44:35 +0100

    refactore seldon module and update output.tf without description

--- src/matcha_ml/infrastructure/resource_group/output.tf
+++ src/matcha_ml/infrastructure/resource_group/output.tf
@@ -1,3 +1,4 @@
 output "name" {
+  description = "Name of the resource group"
   value = azurerm_resource_group.main.name
 }
--- src/matcha_ml/infrastructure/seldon/outputs.tf
+++ src/matcha_ml/infrastructure/seldon/outputs.tf
@@ -1,11 +1,14 @@
 output "ingress-gateway-spec" {
+  description = "The YAML specification for the Istio ingress gateway"
   value = kubectl_manifest.gateway.live_manifest_incluster
 }
 
 output "workloads_namespace" {
+  description = "The namespace for Seldon workloads"
   value = kubernetes_namespace.seldon-workloads.metadata[0].name
 }
 
 output "base_url" {
+  description = "The base URL of the Seldon deployment"
   value = data.kubernetes_service.seldon_ingress.status.0.load_balancer.0.ingress.0.ip
 }
