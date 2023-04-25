1a73b72c2c6b70734de1f7a0c75aee9ed19e1e1d
Oscar <osw282@gmail.com>
2023-04-25 20:10:38 +0100

    sepearte resource group into it's own typer app

--- src/matcha_ml/cli/resources/resource_group.py
+++ src/matcha_ml/cli/resources/resource_group.py
@@ -1,27 +1,27 @@
-"""Get command CLI."""
+"""Subcommands of resource group."""
 import typer
 
-from matcha_ml.cli.resources import experiment_tracker, resource_group
 from matcha_ml.cli.ui.print_messages import print_resource_output
 from matcha_ml.cli.ui.resource_message_builders import build_resource_output
 from matcha_ml.services.matcha_state import MatchaStateService
 
 app = typer.Typer()
 
-# Apps for each command
-app.add_typer(
-    experiment_tracker.app,
-    name="experiment-tracker",
-    help="The experiment-tracker command. Default: prints all information of the experiment tracker provisioned.",
-)
-app.add_typer(
-    resource_group.app,
-    name="resource-group",
-    help="The resource-group command. Default: prints all information of the resource group provisioned.",
-)
-
-
 matcha_state_service = MatchaStateService()
+
+
+@app.command("name")
+def resource_group_name(
+    output: str = typer.Option(
+        default=None,
+    )
+) -> None:
+    """Prints the URL for the experiment tracker provisioned."""
+    resources = matcha_state_service.fetch_resources_from_state_file(
+        ["resource_group_name"]
+    )
+    resource_output = build_resource_output(resources=resources, output_format=output)
+    print_resource_output(resource_output, output_format=output)
 
 
 @app.callback(invoke_without_command=True)
@@ -37,11 +37,13 @@ def default_callback(
         context (typer.Context): data about the current execution
         output (typer.Option): the format of the output specified by the user.
     """
-    # if not check_current_deployment_exists():
-    #     print_error("Error, no resources are currently provisioned.")
-    #     raise typer.Exit()
     if context.invoked_subcommand is None:
-        resources = matcha_state_service.fetch_resources_from_state_file()
+        resource_component_names = [
+            "resource_group_name",
+        ]
+        resources = matcha_state_service.fetch_resources_from_state_file(
+            resource_component_names
+        )
         resource_output = build_resource_output(
             resources=resources, output_format=output
         )
