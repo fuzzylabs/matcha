aba70c9b49ded9aa7b9174d13f5c226be0eb2585
Oscar <osw282@gmail.com>
2023-04-26 16:12:17 +0100

    update based on review comments

--- src/matcha_ml/services/matcha_state.py
+++ src/matcha_ml/services/matcha_state.py
@@ -9,11 +9,21 @@ MATCHA_STATE_DIR = os.path.join(".matcha", "infrastructure", "matcha.state")
 class MatchaStateService:
     """A matcha state service for handling to matcha.state file."""
 
+    state_file_exists = False
+    _state: Optional[Dict[str, str]] = None
+
     def __init__(self) -> None:
         """A constructor for the service which loads the state file."""
-        if os.path.isfile(MATCHA_STATE_DIR):
-            with open(MATCHA_STATE_DIR) as f:
-                self._state = dict(json.load(f))
+        self.state_file_exists = self.check_state_file_exists()
+        self._state = self.state_file
+
+    def check_state_file_exists(self) -> bool:
+        """Check if state file exists.
+
+        Returns:
+            bool: returns True if exists, otherwise False.
+        """
+        return bool(os.path.isfile(MATCHA_STATE_DIR))
 
     @property
     def state_file(self) -> Dict[str, str]:
@@ -22,7 +32,12 @@ class MatchaStateService:
         Returns:
             Dict[str, str]: the state file in the format of a dictionary.
         """
-        return dict(self._state)
+        if self.state_file_exists:
+            with open(MATCHA_STATE_DIR) as f:
+                self._state = dict(json.load(f))
+                return dict(self._state)
+        else:
+            print("Error, matcha.state does not exists")
 
     def fetch_resources_from_state_file(
         self, resource_names: Optional[List[str]] = None
