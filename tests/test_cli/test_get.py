48f1af9f940e262fcd7679126a5838a53a072483
Oscar <osw282@gmail.com>
2023-05-04 16:13:47 +0100

    Implemented the --show-sensitive option for matcha get command

--- tests/test_cli/test_get.py
+++ tests/test_cli/test_get.py
@@ -1,6 +1,7 @@
 """Test suite to test the provision command and all its subcommands."""
 import json
 import os
+from typing import Dict
 
 import pytest
 import yaml
@@ -22,13 +23,21 @@ def mock_state_file(matcha_testing_directory: str):
     os.makedirs(matcha_infrastructure_dir)
 
     state_file_resources = {
-        "cloud": {"flavor": "azure", "resource-group-name": "test_resources"},
+        "pipeline": {
+            "flavor": "zenml",
+            "connection-string": "zenml_test_connection_string",
+            "server-password": "zen_server_password",
+            "server-url": "zen_server_url",
+        },
         "container-registry": {
             "flavor": "azure",
-            "registry-name": "azure_registry_name",
             "registry-url": "azure_container_registry",
         },
         "experiment-tracker": {"flavor": "mlflow", "tracking-url": "mlflow_test_url"},
+        "orchestrator": {
+            "flavor": "aks",
+            "k8s-context": "k8s_test_context",
+        },
     }
 
     with open(os.path.join(matcha_infrastructure_dir, "matcha.state"), "w") as f:
@@ -36,20 +45,49 @@ def mock_state_file(matcha_testing_directory: str):
 
 
 @pytest.fixture
-def expected_outputs() -> dict:
-    """The expected state file initialized.
+def expected_outputs_hide_sensitive() -> Dict[str, Dict[str, str]]:
+    """The expected output from terraform.
 
     Returns:
         dict: expected output
     """
     outputs = {
-        "cloud": {"flavor": "azure", "resource-group-name": "test_resources"},
+        "pipeline": {
+            "flavor": "zenml",
+            "connection-string": "********",
+            "server-password": "********",
+            "server-url": "zen_server_url",
+        },
         "container-registry": {
             "flavor": "azure",
-            "registry-name": "azure_registry_name",
             "registry-url": "azure_container_registry",
         },
         "experiment-tracker": {"flavor": "mlflow", "tracking-url": "mlflow_test_url"},
+        "orchestrator": {"flavor": "aks", "k8s-context": "k8s_test_context"},
+    }
+    return outputs
+
+
+@pytest.fixture
+def expected_outputs_show_sensitive() -> Dict[str, Dict[str, str]]:
+    """The expected output from terraform.
+
+    Returns:
+        dict: expected output
+    """
+    outputs = {
+        "pipeline": {
+            "flavor": "zenml",
+            "connection-string": "********",
+            "server-password": "********",
+            "server-url": "zen_server_url",
+        },
+        "container-registry": {
+            "flavor": "azure",
+            "registry-url": "azure_container_registry",
+        },
+        "experiment-tracker": {"flavor": "mlflow", "tracking-url": "mlflow_test_url"},
+        "orchestrator": {"flavor": "aks", "k8s-context": "k8s_test_context"},
     }
 
     return outputs
@@ -85,6 +123,70 @@ def test_cli_get_command_with_no_state_file(runner: CliRunner):
 
     assert result.exit_code == 0
     assert "Error: matcha.state file does not exist at" in str(result.stdout)
+
+
+def test_cli_get_command_hide_sensitive(runner: CliRunner):
+    """Test cli get command when getting all resources with no `show-sensitive` option specified.
+
+    Args:
+        runner (CliRunner): typer CLI runner
+    """
+    result = runner.invoke(app, ["get"])
+
+    # Exit code 0 means there was no error
+    assert result.exit_code == 0
+
+    expected_output_lines = [
+        "Pipeline",
+        "- flavor: zenml",
+        "- connection-string: ********",
+        "- server-password: *******",
+        "- server-url: zen_server_url",
+        "Container registry",
+        "- flavor: azure",
+        "- registry-url: azure_container_registry",
+        "Experiment tracker",
+        "- flavor: mlflow",
+        "- tracking-url: mlflow_test_url",
+        "Orchestrator",
+        "- flavor: aks",
+        "- k8s-context: k8s_test_context",
+    ]
+
+    for line in expected_output_lines:
+        assert line in result.stdout
+
+
+def test_cli_get_command_show_sensitive(runner: CliRunner):
+    """Test cli for get command when getting all resources with show-sensitive` option specified.
+
+    Args:
+        runner (CliRunner): typer CLI runner
+    """
+    result = runner.invoke(app, ["get", "--show-sensitive"])
+
+    # Exit code 0 means there was no error
+    assert result.exit_code == 0
+
+    expected_output_lines = [
+        "Pipeline",
+        "- flavor: zenml",
+        "- connection-string: zenml_test_connection_string",
+        "- server-password: zen_server_password",
+        "- server-url: zen_server_url",
+        "Container registry",
+        "- flavor: azure",
+        "- registry-url: azure_container_registry",
+        "Experiment tracker",
+        "- flavor: mlflow",
+        "- tracking-url: mlflow_test_url",
+        "Orchestrator",
+        "- flavor: aks",
+        "- k8s-context: k8s_test_context",
+    ]
+
+    for line in expected_output_lines:
+        assert line in result.stdout
 
 
 def test_cli_get_command_with_resource(runner: CliRunner):
@@ -171,14 +273,16 @@ def test_cli_get_command_with_resource_and_property_json(runner: CliRunner):
     assert expected_output in result.stdout
 
 
-def test_cli_get_command_json(runner: CliRunner, expected_outputs: dict):
-    """Test cli get command default in JSON output.
+def test_cli_get_command_json_no_show_sensitive(
+    runner: CliRunner, expected_outputs_hide_sensitive: dict
+):
+    """Test cli get command default in JSON output with no `show-sensitive` option specified.
 
     Args:
         runner (CliRunner): typer CLI runner
-        expected_outputs (dict): expected output as a dict.
+        expected_outputs_hide_sensitive (dict): expected output as a dict.
     """
-    expected_output = json.dumps(expected_outputs, indent=2)
+    expected_output = json.dumps(expected_outputs_hide_sensitive, indent=2)
 
     # Invoke get command
     result = runner.invoke(app, ["get", "--output", "json"])
@@ -189,14 +293,16 @@ def test_cli_get_command_json(runner: CliRunner, expected_outputs: dict):
     assert expected_output in result.stdout
 
 
-def test_cli_get_command_yaml(runner: CliRunner, expected_outputs: dict):
-    """Test cli get command default in YAML output.
+def test_cli_get_command_yaml_no_show_sensitive(
+    runner: CliRunner, expected_outputs_hide_sensitive: dict
+):
+    """Test cli get command default in YAML output with no `show-sensitive` option specified..
 
     Args:
         runner (CliRunner): typer CLI runner
-        expected_outputs (dict): expected output as a dict
+        expected_outputs_hide_sensitive (dict): expected output as a dict
     """
-    expected_output = yaml.dump(expected_outputs, indent=2)
+    expected_output = yaml.dump(expected_outputs_hide_sensitive, indent=2)
 
     # Invoke get command
     result = runner.invoke(app, ["get", "--output", "yaml"])
@@ -205,3 +311,91 @@ def test_cli_get_command_yaml(runner: CliRunner, expected_outputs: dict):
     assert result.exit_code == 0
     # Assert JSON is present and correct in cli output
     assert expected_output in result.stdout
+
+
+def test_cli_get_command_json_show_sensitive(
+    runner: CliRunner, expected_outputs_show_sensitive: dict
+):
+    """Test cli get command default in JSON output with `show-sensitive` option specified..
+
+    Args:
+        runner (CliRunner): typer CLI runner
+        expected_outputs_show_sensitive (dict): expected output as a dict.
+    """
+    expected_output = json.dumps(expected_outputs_show_sensitive, indent=2)
+
+    # Invoke get command
+    result = runner.invoke(app, ["get", "--output", "json"])
+
+    # Exit code 0 means there was no error
+    assert result.exit_code == 0
+    # Assert JSON is present and correct in cli output
+    assert expected_output in result.stdout
+
+
+def test_cli_get_command_yaml_show_sensitive(
+    runner: CliRunner, expected_outputs_show_sensitive: dict
+):
+    """Test cli get command default in YAML output with `show-sensitive` option specified..
+
+    Args:
+        runner (CliRunner): typer CLI runner
+        expected_outputs_show_sensitive (dict): expected output as a dict
+    """
+    expected_output = yaml.dump(expected_outputs_show_sensitive, indent=2)
+
+    # Invoke get command
+    result = runner.invoke(app, ["get", "--output", "yaml"])
+
+    # Exit code 0 means there was no error
+    assert result.exit_code == 0
+    # Assert JSON is present and correct in cli output
+    assert expected_output in result.stdout
+
+
+def test_cli_get_command_no_show_sensitive_with_sensitive_resource(runner: CliRunner):
+    """Test cli get command for when getting resource with sensitive value with no `show-sensitive` option specified.
+
+    Args:
+        runner (CliRunner): typer CLI runner
+    """
+    result = runner.invoke(app, ["get", "pipeline"])
+
+    # Exit code 0 means there was no error
+    assert result.exit_code == 0
+
+    # # Assert string is present in cli output
+    expected_output_lines = [
+        "Pipeline",
+        "- flavor: zenml",
+        "- connection-string: ********",
+        "- server-password: ********",
+        "- server-url: zen_server_url",
+    ]
+
+    for line in expected_output_lines:
+        assert line in result.stdout
+
+
+def test_cli_get_command_show_sensitive_with_resource(runner: CliRunner):
+    """Test cli get command for when getting resource with sensitive value with `show-sensitive` option specified.
+
+    Args:
+        runner (CliRunner): typer CLI runner
+    """
+    result = runner.invoke(app, ["get", "pipeline", "--show-sensitive"])
+
+    # Exit code 0 means there was no error
+    assert result.exit_code == 0
+
+    # # Assert string is present in cli output
+    expected_output_lines = [
+        "Pipeline",
+        "- flavor: zenml",
+        "- connection-string: zenml_test_connection_string",
+        "- server-password: zen_server_password",
+        "- server-url: zen_server_url",
+    ]
+
+    for line in expected_output_lines:
+        assert line in result.stdout
