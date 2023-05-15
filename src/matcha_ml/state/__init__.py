68ed8dfbdf9a3a272bd5d3da75077ce94ac67ac4
D. Lowl <d.lowl@d-lowl.space>
2023-05-15 15:12:08 +0400

    Move MatchaStateService to matcha_ml.state

--- src/matcha_ml/state/__init__.py
+++ src/matcha_ml/state/__init__.py
@@ -1,4 +1,5 @@
 """Matcha state sub-package."""
+from .matcha_state import MatchaStateService
 from .remote_state_manager import RemoteStateManager
 
-__all__ = ["RemoteStateManager"]
+__all__ = ["RemoteStateManager", "MatchaStateService"]
--- src/matcha_ml/templates/build_templates/azure_template.py
+++ src/matcha_ml/templates/build_templates/azure_template.py
@@ -16,7 +16,7 @@ from matcha_ml.cli.ui.status_message_builders import (
     build_substep_success_status,
 )
 from matcha_ml.errors import MatchaPermissionError
-from matcha_ml.services.matcha_state import MatchaStateService
+from matcha_ml.state import MatchaStateService
 
 SUBMODULE_NAMES = [
     "aks",
