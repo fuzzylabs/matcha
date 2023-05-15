1937c538bec3725b1e597d0d21b2d3ab5f2d00fd
D. Lowl <d.lowl@d-lowl.space>
2023-05-15 15:09:42 +0400

    Add Remote State Manager class

---
+++ src/matcha_ml/state/__init__.py
@@ -0,0 +1,4 @@
+"""Matcha state sub-package."""
+from .remote_state_manager import RemoteStateManager
+
+__all__ = ["RemoteStateManager"]
---
+++ src/matcha_ml/state/remote_state_manager.py
@@ -0,0 +1,12 @@
+"""Remote state manager module."""
+
+
+class RemoteStateManager:
+    """Remote State Manager class.
+
+    This class is used to interact with the remote Matcha state.
+    """
+
+    def __init__(self) -> None:
+        """Initialise Remote State Manager."""
+        ...
