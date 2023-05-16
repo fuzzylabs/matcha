42271dc13bdb6ec9a5c00426ece55862d26b4b30
Chris <chris@fuzzylabs.ai>
2023-05-16 10:48:52 +0100

    Revert changes as file was not being overwritten

--- src/matcha_ml/services/global_parameters_service.py
+++ src/matcha_ml/services/global_parameters_service.py
@@ -81,9 +81,12 @@ class GlobalParameters:
             "analytics_opt_out": self._analytics_opt_out,
         }
 
-        with open(self.default_config_file_path, "r+") as file:
+        with open(self.default_config_file_path) as file:
             yaml_data = yaml.safe_load(file)
-            yaml_data.update(data)
+
+        yaml_data.update(data)
+
+        with open(self.default_config_file_path, "w") as file:
             yaml.dump(yaml_data, file)
 
     def opt_out_of_analytics(self) -> None:
