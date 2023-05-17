6b51c9c8546ef705261171b99e747c51609eda32
dudeperf3ct <dudeperf3ct@users.noreply.github.com>
2023-05-17 15:45:21 +0530

    change read mode for upload

--- src/matcha_ml/storage/azure_storage.py
+++ src/matcha_ml/storage/azure_storage.py
@@ -6,9 +6,18 @@ az_connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
 
 
 class AzureStorage:
-    storage_container_name: str
-    # Create the BlobServiceClient object
-    blob_service_client = BlobServiceClient.from_connection_string(az_connection_string)
+    def __init__(self, storage_container_name: str) -> None:
+        self.storage_container_name = storage_container_name
+
+        # Create the BlobServiceClient object
+        self.blob_service_client = BlobServiceClient.from_connection_string(
+            az_connection_string
+        )
+
+        # Get the ContainerClient object for given container name
+        self.container_client = self.blob_service_client.get_container_client(
+            self.storage_container_name
+        )
 
     def upload_file(self, blob_client: ContainerClient, src_file: str):
         """Upload a file to Azure Storage Container.
@@ -17,7 +26,7 @@ class AzureStorage:
             blob_client (ContainerClient): Container client
             src_file (str): Path to upload the file from.
         """
-        with open(src_file, "wb") as blob_data:
+        with open(src_file, "rb") as blob_data:
             blob_client.upload_blob(blob_data)
 
     def upload_folder(self, src_folder_path: str):
@@ -26,15 +35,10 @@ class AzureStorage:
         Args:
             src_folder_path (str): Path to folder to upload all files from
         """
-
-        container_client = self.blob_service_client.get_container_client(
-            self.container_name
-        )
-
         for root, _, filenames in os.walk(src_folder_path):
             for filename in filenames:
                 file_path = os.path.join(root, filename)
-                blob_client = container_client.get_blob_client(blob=file_path)
+                blob_client = self.container_client.get_blob_client(blob=file_path)
                 self.upload_file(blob_client, file_path)
 
     def download_file(self, blob_client: ContainerClient, destination_file: str):
@@ -54,11 +58,7 @@ class AzureStorage:
         Args:
             dest_folder_path (str): Path to folder to download all the files
         """
-        container_client = self.blob_service_client.get_container_client(
-            self.container_name
-        )
-
-        for blob in container_client.list_blobs():
-            blob_client = container_client.get_blob_client(blob.name)
+        for blob in self.container_client.list_blobs():
+            blob_client = self.container_client.get_blob_client(blob.name)
             filename = blob.name.split("/")[-1]
             self.download_file(blob_client, os.path.join(dest_folder_path, filename))
