baea62f2e13d196b72386fd050b9a8d1bfeb6bfd
Chris <chris@fuzzylabs.ai>
2023-05-17 11:07:29 +0100

    Add matcha state UUID to analytics tracking

--- src/matcha_ml/services/analytics_service.py
+++ src/matcha_ml/services/analytics_service.py
@@ -4,6 +4,7 @@ from time import time
 from segment import analytics
 
 from matcha_ml.services.global_parameters_service import GlobalParameters
+from matcha_ml.services.matcha_state import MatchaStateService
 
 analytics.write_key = "qwBKAvY6MEUvv5XIs4rE07ohf5neT3sx"
 
@@ -33,6 +34,18 @@ def track(event_name: str):
             te = time()
 
             global_params = GlobalParameters()
+            matcha_state_service = MatchaStateService()
+
+            # Get the matcha.state UUID if it exists
+            matcha_state_uuid = None
+            if matcha_state_service.check_state_file_exists():
+                if "id" in matcha_state_service.get_resource_names():
+                    if "matcha_uuid" in matcha_state_service.get_property_names("id"):
+                        matcha_state_uuid = (
+                            matcha_state_service.fetch_resources_from_state_file["id"][
+                                "matcha_uuid"
+                            ]
+                        )
 
             if not global_params.analytics_opt_out:
                 analytics.track(
@@ -42,6 +55,7 @@ def track(event_name: str):
                         "time_taken": te - ts,
                         "error_type": f"{error_code.__class__}.{error_code.__class__.__name__}",
                         "command_succeeded": error_code is None,
+                        "matcha_state_uuid": matcha_state_uuid,
                     },
                 )
 
