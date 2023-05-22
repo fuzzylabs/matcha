ab1e0c46d86d1b731f5e19d94f88082bc9193aba
Chris <chris@fuzzylabs.ai>
2023-05-22 11:33:44 +0100

    Clean validation method structure

--- src/matcha_ml/cli/_validation.py
+++ src/matcha_ml/cli/_validation.py
@@ -227,26 +227,24 @@ def check_current_deployment_exists() -> bool:
     with open(MATCHA_STATE_DIR) as f:
         data = json.load(f)
 
-    resource_group_name = None
-
     # Check if a resource group name prefix is present in matcha.state file
     if data.get("cloud") is not None and "prefix" in data.get("cloud"):
         resource_group_name = data["cloud"]["prefix"] + "-resources"
+
+        client = get_azure_client()
+        rg_state = client.resource_group_state(resource_group_name)
+
+        if rg_state is None:
+            return False
+        elif rg_state == ProvisionState.SUCCEEDED:
+            return True
+        else:
+            print_error(
+                f"Error, resource group '{resource_group_name}' is currently in the state '{rg_state.value}' which is currently not handled by matcha. Please check your resources on Azure."
+            )
+            return True
     else:
         return False
-
-    client = get_azure_client()
-    rg_state = client.resource_group_state(resource_group_name)
-
-    if rg_state is None:
-        return False
-    elif rg_state == ProvisionState.SUCCEEDED:
-        return True
-    else:
-        print_error(
-            f"Error, resource group '{resource_group_name}' is currently in the state '{rg_state.value}' which is currently not handled by matcha. Please check your resources on Azure."
-        )
-        return True
 
 
 def get_command_validation(argument: str, valid_options: List[str], noun: str) -> None:
