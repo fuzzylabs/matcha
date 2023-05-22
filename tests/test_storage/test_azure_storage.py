76d04c5881944b324c667a573f501b7af661265d
dudeperf3ct <dudeperf3ct@users.noreply.github.com>
2023-05-22 15:03:20 +0530

    change order

--- tests/test_storage/test_azure_storage.py
+++ tests/test_storage/test_azure_storage.py
@@ -27,36 +27,6 @@ def mock_blob_service() -> BlobServiceClient:
         yield mock_blob_service
 
 
-def test_upload_file(
-    mock_blob_service: BlobServiceClient,
-    matcha_testing_directory: str,
-    mocked_azure_client: AzureClient,
-):
-    """Test AzureStorage upload file function.
-
-    Args:
-        mock_blob_service (BlobServiceClient): Mocked blob service client
-        matcha_testing_directory (str): Temporary directory
-        mocked_azure_client (AzureClient): mocked azure client
-    """
-    test_file_path = os.path.join(matcha_testing_directory, "temp.txt")
-    with open(test_file_path, "w"):
-        pass
-
-    # Mock container client
-    mock_container_client = mock_blob_service.get_container_client.return_value
-
-    # Mock blob client
-    mock_blob_client = mock_container_client.get_blob_client.return_value
-
-    mock_az_storage = AzureStorage("testaccount", "test-rg")
-    mock_az_storage.az_client = mocked_azure_client
-    mock_az_storage.upload_file(mock_blob_client, test_file_path)
-
-    # Check if upload_blob function is called exactly once
-    mock_blob_client.upload_blob.assert_called_once()
-
-
 def test_container_exists(
     mock_blob_service: BlobServiceClient, mocked_azure_client: AzureClient
 ):
@@ -99,6 +69,36 @@ def test_container_does_not_exists(
     mock_az_storage = AzureStorage("testaccount", "test-rg")
     mock_az_storage.az_client = mocked_azure_client
     assert mock_az_storage.container_exists("testcontainer")
+
+
+def test_upload_file(
+    mock_blob_service: BlobServiceClient,
+    matcha_testing_directory: str,
+    mocked_azure_client: AzureClient,
+):
+    """Test AzureStorage upload file function.
+
+    Args:
+        mock_blob_service (BlobServiceClient): Mocked blob service client
+        matcha_testing_directory (str): Temporary directory
+        mocked_azure_client (AzureClient): mocked azure client
+    """
+    test_file_path = os.path.join(matcha_testing_directory, "temp.txt")
+    with open(test_file_path, "w"):
+        pass
+
+    # Mock container client
+    mock_container_client = mock_blob_service.get_container_client.return_value
+
+    # Mock blob client
+    mock_blob_client = mock_container_client.get_blob_client.return_value
+
+    mock_az_storage = AzureStorage("testaccount", "test-rg")
+    mock_az_storage.az_client = mocked_azure_client
+    mock_az_storage.upload_file(mock_blob_client, test_file_path)
+
+    # Check if upload_blob function is called exactly once
+    mock_blob_client.upload_blob.assert_called_once()
 
 
 def test_upload_folder(
