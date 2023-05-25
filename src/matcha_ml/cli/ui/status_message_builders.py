1cfb79f581c2c154284e6d0fce08430624b62cd3
D. Lowl <d.lowl@d-lowl.space>
2023-05-25 18:12:52 +0500

    [RPD-146] Lock and unlock remote state (#122)
    * Add locking and unlocking to the remote state manager
    
    * use_lock in provision, destroy and get
    
    * Address comments

--- src/matcha_ml/cli/ui/status_message_builders.py
+++ src/matcha_ml/cli/ui/status_message_builders.py
@@ -69,3 +69,17 @@ def build_substep_success_status(status: str) -> str:
         str: formatted message
     """
     return f"[green]{status}[/green]"
+
+
+def build_warning_status(status: str) -> str:
+    """Build warning status message.
+
+    Use yellow colour for formatting
+
+    Args:
+        status: status message
+
+    Returns:
+        str: formatted message
+    """
+    return f"[yellow]{status}[/yellow]"
