3d36f5746a8b36a4cd8e8d00e93402fa9533ed4a
Oscar <osw282@gmail.com>
2023-05-25 11:29:43 +0100

    Update based on review comments

--- src/matcha_ml/cli/provision.py
+++ src/matcha_ml/cli/provision.py
@@ -107,7 +107,7 @@ def provision_resources(
         )
         azure_template.build_template(config, template, destination, verbose)
 
-    # Initialises the infrastructure provisioning process.
+    # Initializes the infrastructure provisioning process.
     if template_runner.is_approved(verb="provision"):
         # provision resources by running the template
         template_runner.provision()
