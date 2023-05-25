7dbf0e648d785b554a5482ebe99d92ccc7b9ebab
Chris <chris@fuzzylabs.ai>
2023-05-25 14:02:33 +0100

    Add terraform outputs and update matcha.state when destroy is run

--- src/matcha_ml/cli/cli.py
+++ src/matcha_ml/cli/cli.py
@@ -23,6 +23,7 @@ from matcha_ml.core import core
 from matcha_ml.errors import MatchaError, MatchaInputError
 from matcha_ml.services.analytics_service import AnalyticsEvent, track
 from matcha_ml.state import RemoteStateManager
+from matcha_ml.state.matcha_state import MatchaStateService
 
 app = typer.Typer(no_args_is_help=True, pretty_exceptions_show_locals=False)
 analytics_app = typer.Typer(no_args_is_help=True, pretty_exceptions_show_locals=False)
@@ -121,6 +122,8 @@ def destroy(
 
     if full:
         remote_state_manager.deprovision_state_storage()
+        matcha_state_service = MatchaStateService()
+        matcha_state_service.remove_matcha_state_file()
 
 
 def version_callback(value: bool) -> None:
--- tests/test_cli/test_destroy.py
+++ tests/test_cli/test_destroy.py
@@ -1,10 +1,12 @@
 """Test suite to test the destroy command and all its subcommands."""
+import json
 import os
 from unittest.mock import patch
 
 import pytest
 
 from matcha_ml.cli.cli import app
+from matcha_ml.templates.azure_template.run_azure_template import AzureTemplateRunner
 
 
 @pytest.fixture(autouse=True)
@@ -54,3 +56,65 @@ def test_cli_destroy_command_with_no_provisioned_resources(
         "Error - you cannot destroy resources that have not been provisioned yet."
         in result.stdout
     )
+
+
+def test_cli_destroy_command_updates_matcha_state(runner, matcha_testing_directory):
+    """Test provision command updates the matcha state file when resources (excluding resource group and state bucket) are destroyed.
+
+    Args:
+        runner (CliRunner): typer CLI runner
+        matcha_testing_directory (str): temporary working directory.
+    """
+    os.chdir(matcha_testing_directory)
+
+    matcha_infrastructure_dir = os.path.join(".matcha", "infrastructure", "resources")
+    matcha_state_file_path = os.path.join(".matcha", "infrastructure", "matcha.state")
+
+    os.makedirs(matcha_infrastructure_dir)
+
+    state_file_resources = {
+        "cloud": {
+            "prefix": "test",
+            "location": "ukwest",
+            "flavor": "azure",
+            "resource-group-name": "test_resources",
+        },
+        "id": {"matcha_uuid": "TestStateID"},
+        "container-registry": {
+            "flavor": "azure",
+            "registry-name": "azure_registry_name",
+            "registry-url": "azure_container_registry",
+        },
+        "experiment-tracker": {"flavor": "mlflow", "tracking-url": "mlflow_test_url"},
+    }
+
+    with open(matcha_state_file_path, "w") as f:
+        json.dump(state_file_resources, f)
+
+    expected_matcha_state_contents = {
+        "cloud": {
+            "prefix": "test",
+            "location": "ukwest",
+            "flavor": "azure",
+            "resource-group-name": "test_resources",
+        },
+        "id": {"matcha_uuid": "TestStateID"},
+    }
+
+    # Invoke destroy command
+    with patch(
+        "matcha_ml.cli.destroy.check_current_deployment_exists"
+    ) as check_deployment_exists, patch.object(
+        AzureTemplateRunner, "state_file", matcha_state_file_path
+    ):
+        check_deployment_exists.return_value = True
+        runner.invoke(
+            app,
+            ["destroy"],
+            input="Y\n",
+        )
+
+    with open(matcha_state_file_path) as f:
+        matcha_state_data = json.load(f)
+
+    assert matcha_state_data == expected_matcha_state_contents
