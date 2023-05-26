9973739598423f0616b329de59aa69318ed27c2e
Chris <chris@fuzzylabs.ai>
2023-05-26 09:31:04 +0100

    Update matcha state path variable name for clarity

--- src/matcha_ml/cli/_validation.py
+++ src/matcha_ml/cli/_validation.py
@@ -17,7 +17,7 @@ from matcha_ml.services import AzureClient
 LONGEST_RESOURCE_NAME = "artifactstore"
 MAXIMUM_RESOURCE_NAME_LEN = 24
 
-MATCHA_STATE_DIR = os.path.join(
+MATCHA_STATE_PATH = os.path.join(
     ".matcha", "infrastructure", "resources", "matcha.state"
 )
 
@@ -223,10 +223,10 @@ def check_current_deployment_exists() -> bool:
     Returns:
         bool: True if a deployment currently exists, else False.
     """
-    if not os.path.isfile(MATCHA_STATE_DIR):
+    if not os.path.isfile(MATCHA_STATE_PATH):
         return False
 
-    with open(MATCHA_STATE_DIR) as f:
+    with open(MATCHA_STATE_PATH) as f:
         data = json.load(f)
 
     # Check if a resource group name prefix is present in matcha.state file
