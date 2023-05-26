f54840bce309de48bf284119c037b3808ac40c69
dudeperf3ct <dudeperf3ct@users.noreply.github.com>
2023-05-26 17:26:20 +0530

    test warning

--- tests/test_core/test_core.py
+++ tests/test_core/test_core.py
@@ -1,7 +1,7 @@
 """Test suite to test the core matcha commands and all its subcommands."""
 import json
 import os
-from typing import Dict, Union
+from typing import Dict, Union, Iterator
 from unittest import mock
 
 import pytest
@@ -11,6 +11,7 @@ from matcha_ml.cli.cli import app
 from matcha_ml.core.core import get, remove_state_lock
 from matcha_ml.errors import MatchaError, MatchaInputError
 from matcha_ml.services.global_parameters_service import GlobalParameters
+from matcha_ml.state.remote_state_manager import LOCK_FILE_NAME
 
 INTERNAL_FUNCTION_STUB = "matcha_ml.services.global_parameters_service.GlobalParameters"
 
@@ -68,6 +69,19 @@ def teardown_singleton():
 
 
 @pytest.fixture
+def mock_azure_storage_instance() -> Iterator[mock.MagicMock]:
+    """Mock Azure Storage instance.
+
+    Yields:
+        MagicMock: of Azure Storage instance
+    """
+    class_stub = "matcha_ml.state.remote_state_manager.AzureStorage"
+    with mock.patch(class_stub) as mock_azure_storage:
+        mock_azure_storage_instance = mock_azure_storage.return_value
+        yield mock_azure_storage_instance
+
+
+@pytest.fixture
 def expected_configuration() -> Dict[str, Union[str, bool]]:
     """Pytest fixture to return expected configuration.
 
@@ -75,13 +89,6 @@ def expected_configuration() -> Dict[str, Union[str, bool]]:
         Dict[str, Union[str, bool]]: Dictionary containing expected configuration
     """
     return {"analytics_opt_out": False, "user_id": "dummy_user_id"}
-
-
-@pytest.fixture
-def mock_unlock():
-    """Mock unlock function in RemoteStateManger class"""
-    with mock.patch("matcha_ml.cli.provision.RemoteStateManager.unlock") as mock_unlock:
-        yield mock_unlock
 
 
 def test_get_resources(expected_outputs: dict):
@@ -210,13 +217,27 @@ def test_opt_in_subcommand(
         assert dict(yaml.safe_load(f)) == expected_configuration
 
 
-def test_remove_state_lock_function(mock_unlock):
-    """Test that the unlock function is called once when emove_state_lock function is used.
+def test_remove_state_lock_function():
+    """Test that the unlock function is called once when emove_state_lock function is used."""
+    with mock.patch("matcha_ml.core.core.RemoteStateManager.unlock") as mock_unlock:
+        remove_state_lock()
 
-    Args:
-        mock_unlock: pytest fixture to force unlock state file
-    """
-    remove_state_lock()
+    # Check if unlock function from RemoteStateManager class is called only once
+    mock_unlock.assert_called_once()
 
-    # Check if unlock function from RemoteStateManager class is called
+
+def test_remove_state_lock_function_warning(mock_azure_storage_instance):
+    """Test that the unlock function is called once when emove_state_lock function is used."""
+    with mock.patch("matcha_ml.core.core.RemoteStateManager.unlock") as mock_unlock:
+        mock_azure_storage_instance.blob_exists.return_value = False
+        mock_azure_storage_instance.delete_blob.side_effect = Exception(
+            "Does not exist"
+        )
+
+        remove_state_lock()
+
+        # Check if delete_blob function from AzureStorage class is not called
+        mock_azure_storage_instance.delete_blob.assert_not_called()
+
+    # Check if unlock function from RemoteStateManager class is called only once
     mock_unlock.assert_called_once()
