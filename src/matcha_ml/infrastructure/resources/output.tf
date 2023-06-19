d7e400167833657710b3acdc8d165eeac2a2ace2
Chris <chris@fuzzylabs.ai>
2023-06-19 10:31:52 +0100

    Update terraform output spacing and description

--- src/matcha_ml/infrastructure/resources/output.tf
+++ src/matcha_ml/infrastructure/resources/output.tf
@@ -3,8 +3,8 @@ output "experiment_tracker_mlflow_tracking_url" {
   value       = module.mlflow.mlflow_tracking_url
 }
 
-output "experiment_tracker_mlflow_azure_connection_string"{
-  description = "The Azure connection string for the MLflow storage on Azure"
+output "experiment_tracker_mlflow_azure_connection_string" {
+  description = "The Azure connection string for the MLflow artifact storage"
   value       = module.storage.primary_connection_string
   sensitive   = true
 }
