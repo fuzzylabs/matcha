8a98583a086d2908497601f8a40b0c8c960f7745
Josh <josh.kirsopp@gmail.com>
2023-08-07 16:31:06 +0100

    tests

--- tests/test_cli/test_stack.py
+++ tests/test_cli/test_stack.py
@@ -1,8 +1,10 @@
 """Test suit to test the stack command and all its subcommands."""
 
+import os
 from typer.testing import CliRunner
 
 from matcha_ml.cli.cli import app
+from matcha_ml.config import MatchaConfigService, MatchaConfig
 
 INTERNAL_FUNCTION_STUB = "matcha_ml.core"
 
@@ -83,19 +85,42 @@ def test_stack_set_stack_not_recognized(runner: CliRunner) -> None:
     assert "random is not a valid stack type" in result.stdout
 
 
-def test_stack_set_file_created(matcha_testing_directory) -> None:
+def test_stack_set_file_created(matcha_testing_directory: str, runner: CliRunner) -> None:
     """Test that stack_set cli command creates a config file if one doesn't exist.
 
     Args:
         matcha_testing_directory (str): a temporary working directory.
     """
-    pass
+    os.chdir(matcha_testing_directory)
+    result = runner.invoke(app, ["stack", "set", "llm"])
+    assert result.exit_code == 0
+
+    config = MatchaConfigService.read_matcha_config()
+    assert config.to_dict() == {"stack": {"name": "LLM"}}
 
 
-def test_stack_set_file_modified(matcha_testing_directory) -> None:
+def test_stack_set_file_modified(matcha_testing_directory, mocked_matcha_config_json_object, runner: CliRunner) -> None:
     """Test that if a config file exists, stack set cli command modifies the file.
 
     Args:
         matcha_testing_directory (str): temporary working directory.
     """
-    pass
+    os.chdir(matcha_testing_directory)
+
+    config = MatchaConfig.from_dict(
+        mocked_matcha_config_json_object
+    )
+    config_dict = config.to_dict()
+    MatchaConfigService.write_matcha_config(config)
+
+    result = runner.invoke(app, ["stack", "set", "llm"])
+    assert result.exit_code == 0
+
+    new_config = MatchaConfigService.read_matcha_config()
+
+    new_config_dict = new_config.to_dict()
+
+    assert len(new_config_dict) == len(config_dict) + 1
+    assert "stack" in new_config_dict
+    assert new_config_dict["stack"]["name"] == "LLM"
+    assert config_dict.items() <= new_config_dict.items()
