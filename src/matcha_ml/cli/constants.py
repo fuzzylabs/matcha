bbec0f5e861314948a4ca6bd0c673ff1c1fa706c
Jonathan Carlton <jonathan.carlton@hotmail.com>
2023-09-01 11:18:31 +0100

    RPD-315 removed old resource message constant variable

--- src/matcha_ml/cli/constants.py
+++ src/matcha_ml/cli/constants.py
@@ -1,21 +1,4 @@
 """Constants for use within the Matcha CLI."""
-RESOURCE_MSG = [
-    ("Azure Kubernetes Service (AKS)", "A kubernetes cluster"),
-    (
-        "Two Storage Containers",
-        "A storage container for experiment tracking artifacts and a second for model training artifacts",
-    ),
-    (
-        "Seldon Core",
-        "A framework for model deployment on top of a kubernetes cluster",
-    ),
-    (
-        "Azure Container Registry",
-        "A container registry for storing docker images",
-    ),
-    ("ZenServer", "A zenml server required for remote orchestration"),
-]
-
 RESOURCE_MSG_CORE = [
     ("Azure Kubernetes Service (AKS)", "A kubernetes cluster"),
     (
