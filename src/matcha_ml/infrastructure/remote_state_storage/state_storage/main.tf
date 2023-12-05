426c6e97f1fb23a7e4e704fdaaa26e4b5d5873e4
Christopher-Norman <32800386+Christopher-Norman@users.noreply.github.com>
2023-12-05 13:09:28 +0000

    Change remote state storage to private

--- src/matcha_ml/infrastructure/remote_state_storage/state_storage/main.tf
+++ src/matcha_ml/infrastructure/remote_state_storage/state_storage/main.tf
@@ -16,5 +16,5 @@ resource "azurerm_storage_account" "statestorageaccount" {
 resource "azurerm_storage_container" "statestoragecontainer" {
   name                  = "${var.prefix}statestore"
   storage_account_name  = azurerm_storage_account.statestorageaccount.name
-  container_access_type = "container"
+  container_access_type = "private"
 }
